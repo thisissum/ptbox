f936557cd64f2c8d446963f75dec952e84f81e91
thisissum <thisissunmh@gmail.com>
2021-04-27 16:32:37 +0800

    update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,6 +1,14 @@
+from collections.abc import Iterable
+
 import torch
 from torch import nn
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import (
+    accuracy_score, 
+    f1_score, 
+    roc_auc_score,
+    mean_squared_error, 
+    mean_absolute_error
+)
 
 
 class Metric(object):
@@ -9,23 +17,21 @@ class Metric(object):
     Overwrite function: compute_metric, return metric of updated samples
     Overwrite function: update_state, save listed data into pred_record and true_record
     """
-
-    def __init__(self, name=None, is_validation_metric=False):
-        self.pred_record = list()
-        self.true_record = list()
+    NAME = "metric"
+    def __init__(self):
+        self.pred_record = []
+        self.true_record = []
         self.cur_metric = None
-        self.name = name
-        self.is_validation_metric = is_validation_metric
-
+    
     @property
     def name(self):
-        return self.name
+        return self.NAME
 
     def update_state(self, y_pred, y_true):
-        pass
+        raise NotImplementedError
 
-    def compute_metric(self):
-        pass
+    def compute_metric(self) -> float:
+        raise NotImplementedError
 
     def result(self):
         self.cur_metric = self.compute_metric()
@@ -35,25 +41,17 @@ class Metric(object):
         cur_metric = self.result()
         return "Metric {}: {}".format(self.name, cur_metric)
 
-    def get_cur_metric(self):
-        if self.cur_metric is None:
-            return self.result()
-        return self.cur_metric
-
     def clear(self):
-        self.pred_record = list()
-        self.true_record = list()
+        self.pred_record = []
+        self.true_record = []
         self.cur_metric = None
 
 
 class MetricList(Metric):
+    NAME = "metric_list"
     def __init__(self, metrics):
-        super(MetricList, self).__init__(name='metric_list')
-        self.metrics = metrics if isinstance(metrics, (list, tuple)) else [metrics]
-        self._check()
-        for m in metrics:
-            if m.is_validation_metric:
-                self.validation_metric = m.name
+        super(MetricList, self).__init__()
+        self.metrics = {m.name: m for m in metrics} if isinstance(metrics, Iterable) else {metrics.name: metrics}
 
     def update_state(self, y_pred, y_true):
         for metric in self.metrics:
@@ -62,11 +60,11 @@ class MetricList(Metric):
     def result(self):
         metrics_dict = dict()
         for metric in self.metrics:
-            metrics_dict[metric.name] = metric.result()
+            metrics_dict[metric.metric_name] = metric.result()
         return metrics_dict
 
     def display(self):
-        return "\t".format(metric.display() for metric in self.metrics)
+        return "\t".join(metric.display() for metric in self.metrics)
 
 
     def clear(self):
@@ -78,9 +76,9 @@ class AccuracyMetric(Metric):
     """
     Compute accuracy of all sample updated in
     """
-
+    NAME = "accuracy"
     def __init__(self, normalize=True, sample_weight=None):
-        super(AccuracyMetric, self).__init__(name='accuracy')
+        super(AccuracyMetric, self).__init__()
         self.normalize = normalize
         self.sample_weight = sample_weight
 
@@ -104,9 +102,9 @@ class F1Metric(Metric):
     """
     Compute F1 score of all sample updated in
     """
-
+    NAME = "F1"
     def __init__(self, labels=None, pos_label=1, average='binary', sample_weight=None):
-        super(F1Metric, self).__init__(name='{} f1'.format(average))
+        super(F1Metric, self).__init__()
         self.labels = labels
         self.pos_label = pos_label
         self.average = average
@@ -128,3 +126,104 @@ class F1Metric(Metric):
             sample_weight=self.sample_weight
         )
         return score
+
+
+class AUCMetric(Metric):
+    """
+    Compute roc_auc score of all sample updated in
+    """
+    NAME = "auc"
+    def __init__(self, average="macro", sample_weight=None, max_fpr=None, multi_class="raise", labels=None):
+        super(AUCMetric, self).__init__()
+        self.average = average
+        self.sample_weight = sample_weight
+        self.max_fpr = max_fpr
+        self.multi_class = "raise"
+        self.labels = labels
+    
+    def update_state(self, y_pred, y_true):
+        y_pred = y_pred.argmax(dim=-1).cpu().view(-1).tolist()
+        y_true = y_true.cpu().view(-1).tolist()
+        self.pred_record += y_pred
+        self.true_record += y_true
+
+    def compute_metric(self):
+        score = roc_auc_score(
+            self.true_record, 
+            self.pred_record, 
+            average=self.average, 
+            sample_weight=self.sample_weight, 
+            max_fpr=self.max_fpr, 
+            multi_class=self.multi_class, 
+            labels=self.labels
+        )
+        return score
+
+
+
+class MSEMetric(Metric):
+    """
+    Compute Mean Square Error for regression task
+    """
+    NAME = "mse"
+    def __init__(self, sample_weight=None, multioutput='uniform_average', squared=True):
+        super(MSEMetric, self).__init__()
+        self.sample_weight = sample_weight
+        self.multioutput = multioutput
+        self.squared = squared
+    
+    def update_state(self, y_pred, y_true):
+        y_pred = y_pred.cpu().view(-1).tolist()
+        y_true = y_true.cpu().view(-1).tolist()
+        self.pred_record += y_pred
+        self.true_record += y_true
+    
+    def compute_metric(self):
+        score = mean_squared_error(
+            self.true_record, 
+            self.pred_record, 
+            sample_weight=self.sample_weight,
+            multioutput=self.multioutput,
+            squared=self.squared
+        )
+        return score
+
+
+class MAEMetric(Metric):
+    """
+    Compute Mean Absolute Error for regression task
+    """
+    NAME = "mae"
+    def __init__(self, sample_weight=None, multioutput='uniform_average'):
+        super(MAEMetric, self).__init__()
+        self.sample_weight = sample_weight
+        self.multioutput = multioutput
+    
+    def update_state(self, y_pred, y_true):
+        y_pred = y_pred.cpu().view(-1).tolist()
+        y_true = y_true.cpu().view(-1).tolist()
+        self.pred_record += y_pred
+        self.true_record += y_true
+    
+    def compute_metric(self):
+        score = mean_absolute_error(
+            self.true_record, 
+            self.pred_record, 
+            sample_weight=self.sample_weight,
+            multioutput=self.multioutput
+        )
+        return score
+
+
+METRIC_DICT = {
+    "f1":F1Metric,
+    "accuracy": AccuracyMetric,
+    "roc_auc": AUCMetric,
+    "mse": MSEMetric,
+    "mae": MAEMetric
+}
+
+def get_metric(name: str, *args, **kwargs) -> Metric:
+    """Return specific metric instance by given name.
+    """
+    return METRIC_DICT.get(name, None)(*args, **kwargs)
--- train.py
+++ train.py
@@ -9,8 +9,8 @@ from torch.optim.lr_scheduler import LambdaLR
 
 class ConstantLRWithWarmup(LambdaLR):
     """
-    Increase lr to optimizer.lr linearly with ratio cur_step/num_warmup_steps \n
-    If cur_step >= num_warmup_steps, the ratio will be 1.0 \n
+    Increase lr to optimizer.lr linearly with ratio cur_step/num_warmup_steps  
+    If cur_step >= num_warmup_steps, the ratio will be 1.0  
     Example:
         >>> scheduler = ConstantLRWithWarmup(optimizer, num_warmup_steps, last_epoch)
         ...
@@ -20,21 +20,19 @@ class ConstantLRWithWarmup(LambdaLR):
 
     """
     def __init__(self, optimizer, num_warmup_steps, last_epoch=-1):
-
-        optimizer.zero_grad()
-        optimizer.step()
-
         def lr_lambda(cur_step):
             if cur_step < num_warmup_steps:
                 return float(cur_step) / float(max(num_warmup_steps, 1.0))
             return 1.0
         super(ConstantLRWithWarmup, self).__init__(optimizer, lr_lambda, last_epoch)
+        optimizer.zero_grad()
+        optimizer.step()
 
 
 class ExpDecayLRWithWarmup(LambdaLR):
     """
-    Increase lr to optimizer.lr linearly with ratio cur_step/num_warmup_steps \n
-    If cur_step >= num_warmup_steps, the optimizer.lr will be multiplied by decay_factor for each step \n
+    Increase lr to optimizer.lr linearly with ratio cur_step/num_warmup_steps  
+    If cur_step >= num_warmup_steps, the optimizer.lr will be multiplied by decay_factor for each step  
     Example:
         >>> scheduler = ExpDecayLRWithWarmup(optimizer, num_warmup_steps, decay_factor, last_epoch)
         ...
@@ -43,16 +41,14 @@ class ExpDecayLRWithWarmup(LambdaLR):
         >>> scheduler.step()
     """
     def __init__(self, optimizer, num_warmup_steps, decay_factor=0.9995, last_epoch=-1):
-
-        optimizer.zero_grad()
-        optimizer.step()
-
         def lr_lambda(cur_step):
             if cur_step < num_warmup_steps:
                 return float(cur_step) / float(max(num_warmup_steps, 1.0))
             times = min(max(cur_step, 1.0) - num_warmup_steps, 1.0)
             return decay_factor**times
         super(ExpDecayLRWithWarmup, self).__init__(optimizer, lr_lambda, last_epoch)
+        optimizer.zero_grad()
+        optimizer.step()
 
 
 class GradientAccumulator(object):
@@ -67,13 +63,14 @@ class GradientAccumulator(object):
                 accu.backward()
                 accu.step()
     """
-    def __init__(self, accumulation_steps=1):
+    def __init__(self, accumulation_steps: int = 1):
         self.accumulation_steps = accumulation_steps
         self._cur_step = 0
 
-    def __call__(self, loss, optimizer):
+    def __call__(self, loss, optimizer, lr_scheduler=None):
         self._loss = loss
         self._optimizer = optimizer
+        self._lr_scheduler = lr_scheduler
         return self
 
     def __enter__(self):
@@ -89,7 +86,9 @@ class GradientAccumulator(object):
     
     def step(self):
         if self._cur_step % self.accumulation_steps == 0:
-            # print("backward")
+            # change lr before apply gradients to weights
+            if self._lr_scheduler is not None:
+                self._lr_scheduler.step()
             self._optimizer.step()
             self._optimizer.zero_grad()
     
